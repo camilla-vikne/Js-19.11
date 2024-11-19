609e87104d17827a2ec951d7c07296c3c466865b
camilla-vikne <camillab09.kodehode@gmail.com>
2024-11-19 11:30:07 +0100

    Lagt inn oppgaven til mandag

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ const calculator = (num1, num2, operator) => {
   }
 };
 console.log(calculator(10, 2, "+")); // Output: 12
-*/
+
 
 let testBtn = document.getElementById("test-btn");
 // function testClick() {
@@ -106,3 +106,21 @@ let testBtn = document.getElementById("test-btn");
 testBtn.addEventListener("click", function () {
   console.log("clicked");
 });
+*/
+
+
+Oppgave 1: 
+let userName = "John";
+let userAge = 20;
+let userCity = "Bergen";
+
+Lag en if - else if - else statement som sjekker følgende og console logger:
+1: Om bruker er over 18 år- "Du er over 18, velkommen inn" ,
+2: Om bruker er under eller lik 18 år ELLER er fra Oslo - "Du er ikke gammel nok, og du er fra feil by",
+3: Om bruker heter John OG er fra Bergen - "Velkommen inn, John fra Bergen!"
+
+Oppgave 2 (valgfri):
+Lag en kalkulator med en switch statement som kan regne ut +, -, / og * på to tall
+Husk å console logge for å sjekke at den regner ut riktig!
+
+Frist: Mandag 25.11 kl 14:00. Send link til Github repo- husk å laste opp Github page også!
